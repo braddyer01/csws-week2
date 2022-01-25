058ef0ea223b15c9406298c1d5c65ba33947b3c9
CSMBdyer <braddyer9801@gmail.com>
2022-01-25 14:29:22 +0000

    World.py - Added

---
+++ world.py
@@ -0,0 +1,9 @@
+places = ['USA','Norway','Austria','Canada','Morocco']
+print(places)
+print(sorted(places))
+print(sorted(places, reverse = True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
